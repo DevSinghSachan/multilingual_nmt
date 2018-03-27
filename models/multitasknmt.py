a61098d7aaa720ac2a43a7ebef54cf659bada24f
Devendra Singh <devendra.singh@petuum.com>
2018-03-27 01:23:06 -0400

    BLEU computation in multi-lingual setting
    Former-commit-id: cad3ccbc06eabd8efcdcec8e86095e1b07df175c

--- models/multitasknmt.py
+++ models/multitasknmt.py
@@ -11,17 +11,24 @@ def find_key_from_val(dict_, value):
 
 
 def index_select_train(lang_id, input_):
-    index = torch.nonzero(input_[0][:, 1] == lang_id)[:, 0]
-    x_block = torch.index_select(input_[0], 0, index)
-    y_in_block = torch.index_select(input_[1], 0, index)
-    y_out_block = torch.index_select(input_[2], 0, index)
-    return x_block, y_in_block, y_out_block
+    index = torch.nonzero(input_[0][:, 1] == lang_id)
+    if index.numel() > 0:
+        index = index[:, 0]
+        x_block = torch.index_select(input_[0], 0, index)
+        y_in_block = torch.index_select(input_[1], 0, index)
+        y_out_block = torch.index_select(input_[2], 0, index)
+        return x_block, y_in_block, y_out_block
+    else:
+        return None
 
 
 def index_select_translate(lang_id, input_):
-    index = torch.nonzero(input_[:, 1] == lang_id)[:, 0]
-    x_block = torch.index_select(input_, 0, index)
-    return index, x_block
+    index = torch.nonzero(input_[:, 1] == lang_id)
+    if index.numel() > 0:
+        index = index[:, 0]
+        x_block = torch.index_select(input_, 0, index)
+        return index, x_block
+    return index, input_
 
 
 class MultiTaskNMT(nn.Module):
@@ -56,10 +63,18 @@ class MultiTaskNMT(nn.Module):
     def forward(self, *args):
         # Identify the row indexes corresponding to lang1 and lang2
         lang1_input = index_select_train(self.lang1, args)
-        loss1, stats1 = self.model1(*lang1_input)
+        if lang1_input is not None:
+            loss1, stats1 = self.model1(*lang1_input)
+        else:
+            loss1 = 0.
+            stats1 = utils.Statistics()
 
         lang2_input = index_select_train(self.lang2, args)
-        loss2, stats2 = self.model2(*lang2_input)
+        if lang2_input is not None:
+            loss2, stats2 = self.model2(*lang2_input)
+        else:
+            loss2 = 0.
+            stats2 = utils.Statistics()
 
         n_total = stats1.n_words + stats2.n_words
         n_correct = stats1.n_correct + stats2.n_correct
@@ -73,10 +88,16 @@ class MultiTaskNMT(nn.Module):
     def translate(self, x_block, max_length=50, beam=5, alpha=0.6):
         # Identify the row indexes corresponding to lang1 and lang2
         index1, x_block1 = index_select_translate(self.lang1, x_block)
-        id_list1 = self.model1.translate(x_block1, max_length, beam, alpha)
+        if index1.numel() > 0:
+            id_list1 = self.model1.translate(x_block1, max_length, beam, alpha)
+        else:
+            id_list1 = []
 
         index2, x_block2 = index_select_translate(self.lang2, x_block)
-        id_list2 = self.model2.translate(x_block2, max_length, beam, alpha)
+        if index2.numel() > 0:
+            id_list2 = self.model2.translate(x_block2, max_length, beam, alpha)
+        else:
+            id_list2 = []
 
         index = index1.data.tolist() + index2.data.tolist()
         id_list = id_list1 + id_list2
