fixed bug in exp saving average
Former-commit-id: 8e0f60c03d4cbce0a5dbff97bbd3912984d409d8

models/multitasknmt.py
@@ -51,22 +51,52 @@ class MultiTaskNMT(nn.Module):
         # Query Linear Layer Weight sharing in transformer encoder
         for i in range(config.layers):
             if config.pshare_encoder_param:
-                pass
+                # pass
                 # Share Decoder Layer
-                # self.model1.decoder.layers[i] = self.model2.decoder.layers[i]
+                self.model1.decoder.layers[i] = self.model2.decoder.layers[i]
 
-                # Share Decoder Params
                 # Share Query
-                # self.model1.encoder.layers[i].self_attention.W_Q.weight = \
-                #     self.model2.encoder.layers[i].self_attention.W_Q.weight
+                if 'q' in config.share_sublayer:
+                    if "self" in config.attn_share:
+                        self.model1.encoder.layers[i].self_attention.W_Q.weight = \
+                            self.model2.encoder.layers[i].self_attention.W_Q.weight
+                    if "source" in config.attn_share:
+                        self.model1.encoder.layers[i].source_attention.W_Q.weight = \
+                            self.model2.encoder.layers[i].source_attention.W_Q.weight
 
                 # Share Key
-                # self.model1.encoder.layers[i].self_attention.W_K.weight = \
-                #     self.model2.encoder.layers[i].self_attention.W_K.weight
+                if 'k' in config.share_sublayer:
+                    if "self" in config.attn_share:
+                        self.model1.encoder.layers[i].self_attention.W_K.weight = \
+                            self.model2.encoder.layers[i].self_attention.W_K.weight
+                    if "source" in config.attn_share:
+                        self.model1.encoder.layers[i].source_attention.W_K.weight = \
+                            self.model2.encoder.layers[i].source_attention.W_K.weight
 
                 # Share Value
-                # self.model1.encoder.layers[i].self_attention.W_V.weight = \
-                #     self.model2.encoder.layers[i].self_attention.W_V.weight
+                if 'v' in config.share_sublayer:
+                    if "self" in config.attn_share:
+                        self.model1.encoder.layers[i].self_attention.W_V.weight = \
+                            self.model2.encoder.layers[i].self_attention.W_V.weight
+                    if "source" in config.attn_share:
+                        self.model1.encoder.layers[i].source_attention.W_V.weight = \
+                            self.model2.encoder.layers[i].source_attention.W_V.weight
+
+                # Share last Finishing Linear Layer
+                if 'f' in config.share_sublayer:
+                    if "self" in config.attn_share:
+                        self.model1.encoder.layers[i].self_attention.finishing_linear_layer.weight = \
+                            self.model2.encoder.layers[i].self_attention.finishing_linear_layer.weight
+                    if "source" in config.attn_share:
+                        self.model1.encoder.layers[i].source_attention.finishing_linear_layer.weight = \
+                            self.model2.encoder.layers[i].source_attention.finishing_linear_layer.weight
+
+                # Share the linear layers
+                if 'linear' in config.share_sublayer:
+                    self.model1.encoder.layers[i].feed_forward.W_1.weight = \
+                        self.model2.encoder.layers[i].feed_forward.W_1.weight
+                    self.model1.encoder.layers[i].feed_forward.W_2.weight = \
+                        self.model2.encoder.layers[i].feed_forward.W_2.weight
 
             elif config.pshare_decoder_param:
                 # pass