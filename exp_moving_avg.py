fixed bug in exp saving average

exp_moving_avg.py
@@ -1,3 +1,4 @@
+from torch.autograd import Variable
 
 
 class ExponentialMovingAverage(object):
@@ -18,7 +19,7 @@ class ExponentialMovingAverage(object):
         self.num_updates += 1
         decay = min(self.decay, (1 + self.num_updates) / (10 + self.num_updates))
         for name, param in var_list.items():
-            if param.requires_grad:
+            if isinstance(param, Variable) and param.requires_grad:
                 assert name in self.shadow_variable_dict
                 data = self.shadow_variable_dict[name]
                 data -= (1 - decay) * (data - param.data.clone())