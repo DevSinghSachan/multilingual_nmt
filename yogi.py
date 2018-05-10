b7ad9a845f51f498e7ee07ddf5512a92babae253
Devendra Singh <devendra.singh@petuum.com>
2018-05-09 21:26:43 -0400

    updated yogi

--- yogi.py
+++ yogi.py
@@ -16,11 +16,12 @@ class Yogi(Optimizer):
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-3,
+    def __init__(self, params, lr=1e-2, betas=(0.9, 0.999), eps=1e-3,
                  weight_decay=0):
         defaults = dict(lr=lr, betas=betas, eps=eps,
                         weight_decay=weight_decay)
         super(Yogi, self).__init__(params, defaults)
+        self.v_init = 0.0
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -49,7 +50,7 @@ class Yogi(Optimizer):
                     # Exponential moving average of gradient values
                     state['exp_avg'] = torch.zeros_like(p.data)
                     # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data) + self.v_init
 
                 exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
                 beta1, beta2 = group['betas']
@@ -64,14 +65,14 @@ class Yogi(Optimizer):
 
                 grad2 = grad * grad
                 sign = (exp_avg_sq - grad2).sign_()
-                exp_avg_sq.add_(beta2 - 1, sign * grad2)
+                exp_avg_sq.addcmul_(beta2 - 1, sign, grad2)
 
                 denom = exp_avg_sq.sqrt().add_(group['eps'])
 
                 bias_correction1 = 1 - beta1 ** state['step']
                 bias_correction2 = 1 - beta2 ** state['step']
-                step_size = group[
-                    'lr']  # * math.sqrt(bias_correction2) / bias_correction1
+                step_size = group['lr'] * math.sqrt(
+                    bias_correction2) / bias_correction1
 
                 p.data.addcdiv_(-step_size, exp_avg, denom)
 
