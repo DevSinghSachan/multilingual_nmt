9775adcff9dde3417b5cbb89b7acce3cd8e67841
Devendra Singh Sachan <devendra.singh@petuum.com>
2019-06-12 00:13:42 -0400

    added wmt18 zh->en script

--- ted_reader.py
+++ ted_reader.py
@@ -1,29 +1,26 @@
 import itertools
 import os
 import csv
-import sys
 from collections import defaultdict
 from six.moves import zip
+from argparse import ArgumentParser
 
 
 class MultiLingualAlignedCorpusReader(object):
     """Handles the case of reading TED talk files
     """
-    def __init__(self, corpus_path, vocab=None, delimiter='\t',
-                 target_token=True, bilingual=True, corpus_type='file',
+    def __init__(self, corpus_path,
+                 delimiter='\t',
+                 target_token=True,
                  lang_dict={'source': ['fr'], 'target': ['en']},
-                 zero_shot=False, eval_lang_dict=None):
+                 cartesian_product=True):
         self.empty_line_flag = 'NULL'
         self.corpus_path = corpus_path
         self.delimiter = delimiter
-        self.bilingual = bilingual
         self.lang_dict = lang_dict
         self.lang_set = set()
         self.target_token = target_token
-        self.zero_shot = zero_shot
-        self.eval_lang_dict = eval_lang_dict
-        self.corpus_type = corpus_type
-
+        self.cartesian_product = cartesian_product
         for list_ in self.lang_dict.values():
             for lang in list_:
                 self.lang_set.add(lang)
@@ -60,10 +57,8 @@ class MultiLingualAlignedCorpusReader(object):
 
             if src_sent.find(self.empty_line_flag) != -1 or len(src_sent) == 0:
                 continue
-
             elif sent2.find(self.empty_line_flag) != -1 or len(sent2) == 0:
                 continue
-
             else:
                 data_dict['source'].append(sent1)
                 data_dict['target'].append(sent2)
@@ -91,107 +86,66 @@ class MultiLingualAlignedCorpusReader(object):
             for row in reader:
                 data_dict['source'].append(row[s_lang])
                 data_dict['target'].append(row[t_lang])
-
         if self.target_token:
             text = self.add_target_token(data_dict['source'], t_lang)
             data_dict['source'] = text
-
-        return data_dict['source'], data_dict['target']
-
-    def read_from_directory(self, path_, s_lang, t_lang):
-        data_dict = defaultdict(list)
-
-        for talk_dir in os.listdir(path_):
-            dir_path = os.path.join(path_, talk_dir)
-
-            talk_lang_set = set([l.split('.')[0] for l in os.listdir(dir_path)])
-
-            if s_lang not in talk_lang_set or t_lang not in talk_lang_set:
-                continue
-
-            for infile in os.listdir(dir_path):
-                lang = os.path.splitext(infile)[0]
-
-                if lang in self.lang_set:
-                    file_path = os.path.join(dir_path, infile)
-                    text = self.read_data(file_path)
-
-                    if lang == s_lang:
-                        if self.target_token:
-                            text = self.add_target_token(text, t_lang)
-                            data_dict['source'] += text
-                        else:
-                            data_dict['source'] += text
-
-                    elif lang == t_lang:
-                        data_dict['target'] += text
-
         return data_dict['source'], data_dict['target']
 
     def read_aligned_corpus(self, split_type='train'):
         data_dict = defaultdict(list)
-        iterable = []
-        s_list = []
-        t_list = []
-
-        if self.zero_shot:
-            if split_type == "train":
-                iterable = zip(self.lang_dict['source'], self.lang_dict['target'])
-            else:
-                iterable = zip(self.eval_lang_dict['source'], self.eval_lang_dict['target'])
-
-        elif self.bilingual:
-            iterable = itertools.product(self.lang_dict['source'], self.lang_dict['target'])
+        if self.cartesian_product:
+            iterable = itertools.product(self.lang_dict['source'],
+                                         self.lang_dict['target'])
+        else:
+            iterable = zip(self.lang_dict['source'], self.lang_dict['target'])
 
         for s_lang, t_lang in iterable:
             if s_lang == t_lang:
                 continue
-            if self.corpus_type == 'directory':
-                split_type_path = os.path.join(self.corpus_path, split_type)
-                s_list, t_list = self.read_from_directory(split_type_path, s_lang=s_lang, t_lang=t_lang)
-
-            elif self.corpus_type == 'file':
-                split_type_file_path = os.path.join(self.corpus_path, "all_talks_{}.tsv".format(split_type))
-                s_list, t_list = self.read_from_single_file(split_type_file_path, s_lang=s_lang, t_lang=t_lang)
-
+            split_type_file_path = os.path.join(self.corpus_path, "all_talks_{}.tsv".format(split_type))
+            s_list, t_list = self.read_from_single_file(split_type_file_path, s_lang=s_lang, t_lang=t_lang)
             data_dict['source'] += s_list
             data_dict['target'] += t_list
-
         new_data_dict = self.filter_text(data_dict)
         return new_data_dict
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description='TED talks dataset reader for bilingual/multilingual translation')
+    parser.add_argument('--input', '-i', type=str, default='./data/',
+                        help='TED talks data directory')
+    parser.add_argument('--save_data_dir', type=str, default='./data/',
+                        help='directory to save translation data files')
+    parser.add_argument('--src_langs', '-s',  nargs='+', type=str, default=['en'],
+                        help='Source languages list (e.g. "en", "vi")')
+    parser.add_argument('--tgt_langs', '-t', nargs='+', type=str, default=['vi'],
+                        help='Target languages list (e.g. "en", "vi")')
+    parser.add_argument('--no_cartesian_product', '-ncp', dest='cartesian_product',
+                        action='store_false',
+                        help='Generate multilingual training data from the cartesian product of source and target language sets')
+    parser.set_defaults(cartesian_product=True)
+    args = parser.parse_args()
 
-    # Testing the code
-    data_path = 'data'
-    src_langs = ['ja', 'en']
-    tgt_langs = ['ja', 'en']
-    DATA_DIR = 'data/{}_{}'.format('+'.join(src_langs), '+'.join(tgt_langs))
+    SAVE_DATA_DIR = '{}/{}_{}'.format(args.save_data_dir, '+'.join(args.src_langs), '+'.join(args.tgt_langs))
+    lang_dict={'source': args.src_langs, 'target': args.tgt_langs}
 
-    zs_train_lang_dict={'source': src_langs, 'target': tgt_langs}
-    zs_eval_lang_dict = {'source': src_langs, 'target': tgt_langs}
-
-    obj = MultiLingualAlignedCorpusReader(corpus_path=data_path,
-                                          lang_dict=zs_train_lang_dict,
+    obj = MultiLingualAlignedCorpusReader(corpus_path=args.input,
+                                          lang_dict=lang_dict,
                                           target_token=True,
-                                          corpus_type='file',
-                                          eval_lang_dict=zs_eval_lang_dict,
-                                          zero_shot=False,
-                                          bilingual=True)
+                                          cartesian_product=args.cartesian_product)
 
-    os.makedirs(DATA_DIR, exist_ok=True)
-    obj.save_file(os.path.join(DATA_DIR, 'train.src'),
+    os.makedirs(SAVE_DATA_DIR, exist_ok=True)
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'train.src'),
                   split_type='train', data_type='source')
-    obj.save_file(os.path.join(DATA_DIR, 'train.tgt'),
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'train.tgt'),
                   split_type='train', data_type='target')
 
-    obj.save_file(os.path.join(DATA_DIR, 'test.src'),
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'test.src'),
                   split_type='test', data_type='source')
-    obj.save_file(os.path.join(DATA_DIR, 'test.tgt'),
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'test.tgt'),
                   split_type='test', data_type='target')
 
-    obj.save_file(os.path.join(DATA_DIR, 'dev.src'),
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'dev.src'),
                   split_type='dev', data_type='source')
-    obj.save_file(os.path.join(DATA_DIR, 'dev.tgt'),
+    obj.save_file(os.path.join(SAVE_DATA_DIR, 'dev.tgt'),
                   split_type='dev', data_type='target')
