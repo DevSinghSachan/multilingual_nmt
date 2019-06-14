c02aa2475fbc83cc3ed3ae701a48ef642343a881
Devendra Singh Sachan <devendra.singh@petuum.com>
2019-06-14 14:49:09 -0400

    removed ununsed files
    Former-commit-id: 1f01dde322c7e6c1a4e094d3a0d082325546bdc0

--- api.py
+++
@@ -1,9 +0,0 @@
-from nfseq import translate
-
-sent = '道元（どうげん）は、鎌倉時代初期の禅僧。'
-sent = 'Examples of Koan stories in the beginning.'
-print(sent)
-sent_list = translate(sent=sent, src='en', tgt='ja')
-print(sent_list)
-sent_list = translate(sent=sent_list[0], src='ja', tgt='en')
-print(sent_list)
